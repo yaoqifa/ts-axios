15113851ea857c0e0856612607dcf3ff3bc3451e
yaoqifa <yaoqifa07330@hellobike.com>
2019-07-25 10:54:19 +0800

    function axios and xhr

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type Method = 'get' | 'GET' | 'post' | 'POST' | 'delete' | 'DELETE' |
+  'put' | 'PUT' | 'head' | 'HEAD' | 'options' | 'OPTIONS' | 'patch' | 'PATCH'
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  params?: any
+  data?: any
+}
