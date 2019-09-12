cd297352223d6b97cd695fb27fa2bf2417a96246
yaoqifa <yaoqifa07330@hellobike.com>
2019-09-12 19:40:21 +0800

    add download upload process

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,75 +1,129 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
 import { parseHeaders } from '../helpers/headers'
 import { createError } from '../helpers/error'
+import { isURLSameOrigin } from '../helpers/url';
+import cookie from '../helpers/cookie';
+import { isFormData } from '../helpers/utils';
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
-    const {data = null, method = 'get', url, headers, responseType, timeout, cancelToken, withCredentials } = config
+    const {
+      data = null,
+      method = 'get',
+      url,
+      headers,
+      responseType,
+      timeout,
+      cancelToken,
+      withCredentials,
+      xsrfCookieName,
+      xsrfHeaderName,
+      onDownloadProgress,
+      onUploadProgress
+    } = config
     const request = new XMLHttpRequest()
-    if (responseType) {
-      request.responseType = responseType
-    }
-    if (timeout) {
-      request.timeout = timeout
-    }
-
-    if (withCredentials) {
-      request.withCredentials = withCredentials
-    }
 
     request.open(method.toUpperCase(), url!, true)
 
-    Object.keys(headers).forEach((key) => {
-      if (data === null && key.toLowerCase() === 'content-type') {
-        delete headers[key]
-      } else {
-        request.setRequestHeader(key, headers[key])
-      }
-    })
+    configureRequest()
 
-    request.onreadystatechange = function handleLoad() {
-      if (request.readyState !== 4) {
-        return
+    addEvents()
+
+    processHeaders()
+
+    processCancel()
+
+    request.send(data)
+
+    function configureRequest(): void {
+      if (responseType) {
+        request.responseType = responseType
       }
-      // 只读属性 XMLHttpRequest.status 返回了XMLHttpRequest 响应中的数字状态码。status 的值是一个无符号短整型。
-      // 在请求完成前，status的值为0。值得注意的是，如果 XMLHttpRequest 出错，浏览器返回的 status 也为0。
-      if (request.status === 0) {
-        return
+      if (timeout) {
+        request.timeout = timeout
       }
 
-      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
-      const responseData = responseType !== 'text' ? request.response : request.responseText
-      const response: AxiosResponse = {
-        data: responseData,
-        status: request.status,
-        statusText: request.statusText,
-        headers: responseHeaders,
-        config,
-        request
-      }
-      if (request.status >= 200 && request.status < 300) {
-        resolve(response)
-      } else {
-        reject(createError(`Request faild with status code ${request.status}`, config, null, request, response))
+      if (withCredentials) {
+        request.withCredentials = withCredentials
       }
     }
 
-    // 处理网络错误
-    request.onerror = function handleError() {
-      reject(createError('Network Error', config, null, request))
+    function addEvents(): void {
+      request.onreadystatechange = function handleLoad() {
+        if (request.readyState !== 4) {
+          return
+        }
+        // 只读属性 XMLHttpRequest.status 返回了XMLHttpRequest 响应中的数字状态码。status 的值是一个无符号短整型。
+        // 在请求完成前，status的值为0。值得注意的是，如果 XMLHttpRequest 出错，浏览器返回的 status 也为0。
+        if (request.status === 0) {
+          return
+        }
+
+        const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+        const responseData = responseType !== 'text' ? request.response : request.responseText
+        const response: AxiosResponse = {
+          data: responseData,
+          status: request.status,
+          statusText: request.statusText,
+          headers: responseHeaders,
+          config,
+          request
+        }
+        if (request.status >= 200 && request.status < 300) {
+          resolve(response)
+        } else {
+          reject(createError(`Request faild with status code ${request.status}`, config, null, request, response))
+        }
+      }
+
+      // 处理网络错误
+      request.onerror = function handleError() {
+        reject(createError('Network Error', config, null, request))
+      }
+
+      // 处理超时错误
+      request.ontimeout = function handleTimeout() {
+        reject(createError(`Timeout of ${timeout} ms exceeded `, config, 'ECONNABORTED', request))
+      }
+
+      if (onDownloadProgress) {
+        request.onprogress = onDownloadProgress
+      }
+
+      if (onUploadProgress) {
+        request.upload.onprogress = onUploadProgress
+      }
     }
 
-    // 处理超时错误
-    request.ontimeout = function handleTimeout() {
-      reject(createError(`Timeout of ${timeout} ms exceeded `, config, 'ECONNABORTED', request))
-    }
+    function processHeaders(): void {
+      if (isFormData(data)) {
+        delete headers['Content-Type']
+      }
 
-    if (cancelToken) {
-      cancelToken.promise.then(reason => {
-        request.abort()
-        reject(reason)
+      if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
+        const xsrfValue = cookie.read(xsrfCookieName)
+        if (xsrfValue && xsrfHeaderName) {
+          headers[xsrfHeaderName] = xsrfValue
+        }
+      }
+
+      Object.keys(headers).forEach((key) => {
+        if (data === null && key.toLowerCase() === 'content-type') {
+          delete headers[key]
+        } else {
+          request.setRequestHeader(key, headers[key])
+        }
       })
     }
-    request.send(data)
+
+    function processCancel(): void {
+      if (cancelToken) {
+        cancelToken.promise.then(reason => {
+          request.abort()
+          reject(reason)
+        })
+      }
+    }
+
   })
 }
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -44,3 +44,7 @@ export function deepMerge(...objs: any[]): any {
 export function isURLSearchParams(val: any): val is URLSearchParams {
   return typeof val !== undefined && val instanceof URLSearchParams
 }
+
+export function isFormData(val: any): val is FormData {
+  return typeof val !== 'undefined' && val instanceof FormData
+}
